e0b7ef7771f332eba6fc080aa1456c4327d6c8f4
Felix Trepanier <felixtrepanier@gmail.com>
2023-08-28 17:31:40 -0400

    initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'md-responsive';
+  data = [
+    {
+      title: "Carotte",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Carrots_on_Display.jpg/240px-Carrots_on_Display.jpg",
+    },
+    {
+      title: "Radis",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/0/06/Radis_ronds.JPG",
+    },
+    {
+      title: "Navet",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Turnip_2622027.jpg",
+    },
+    {
+      title: "Concombre",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Komkommer_plant.jpg",
+    },
+    {
+      title: "Tomate",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/b/b7/TomateCherryTross.jpg",
+    },
+    {
+      title: "Céleri",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/4/44/C%C3%A9leri.jpg",
+    },
+    {
+      title: "Courgette",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Courgette_Cucurbita_pepo_2.jpg",
+    },
+  ];
+
+  filterControl = this.fb.control("");
+  filteredProducts = this.data;
+
+  constructor(private fb: FormBuilder) {
+    this.filterControl.valueChanges.subscribe((value) => {
+      this.filteredProducts = this.data.filter((product) => {
+        if (value) {
+          return product.title.toLowerCase().includes(value.toLowerCase());
+        } else {
+          return true;
+        }
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
